3aa36d51b5a5bd02e7916fa47cbd6e41ec7509ff
unknown <asheik@zuralabs.com>
2025-01-27 15:55:32 +0530

    initial-commit

---
+++ src/main/java/com/practise/JavaPractiseWithExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPractiseWithExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPractiseWithExamplesApplication.class, args);
+	}
+
+}
